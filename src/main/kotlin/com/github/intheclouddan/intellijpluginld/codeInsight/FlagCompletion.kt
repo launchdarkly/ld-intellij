3f97e07b058119f1dd9e5d28b07834520f13de05
Dan O'Brien <dobrien@launchdarkly.com>
2020-07-02 13:08:27 -0400

    try to match everything

--- src/main/kotlin/com/github/intheclouddan/intellijpluginld/codeInsight/FlagCompletion.kt
+++ src/main/kotlin/com/github/intheclouddan/intellijpluginld/codeInsight/FlagCompletion.kt
@@ -1,46 +1,46 @@
 package com.github.intheclouddan.intellijpluginld.codeInsight
 
+import com.github.intheclouddan.intellijpluginld.FlagStore
 import com.intellij.codeInsight.completion.*
-import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
-import com.intellij.openapi.project.Project
-import com.intellij.patterns.PlatformPatterns
-import com.intellij.util.ProcessingContext
+import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.patterns.PsiJavaPatterns.*
 import com.intellij.psi.JavaTokenType
-import com.github.intheclouddan.intellijpluginld.FlagStore
-import com.intellij.patterns.PsiJavaPatterns.psiLiteral
-import com.intellij.patterns.PsiJavaPatterns.psiElement
-import com.intellij.patterns.PsiJavaPatterns.psiMethod
+
+import com.intellij.util.ProcessingContext
 import com.launchdarkly.api.model.FeatureFlags
-import com.intellij.patterns.PsiJavaPatterns
 
 
 @Service
 class JavaCompletionContributor : CompletionContributor() {
-    var flags: FeatureFlags? = null
+
+    override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
+        val caretOffset: Int = parameters.editor.getCaretModel().getOffset()
+        val elem = parameters.originalFile.findElementAt(caretOffset)
+        println(elem)
+    }
+
+    companion object {
+        val FLAG_KEY = psiElement()
+    }
 
     init {
-
         extend(CompletionType.BASIC,
-                PlatformPatterns.psiElement(JavaTokenType.STRING_LITERAL).withParent(
-                    psiElement().methodCallParameter(
-                            0,
-                    psiMethod()
-                            .withName("boolVariation")
-                            .definedInClass("com.launchdarkly.sdk.server.LDClient")
-                            )),
-                object : CompletionProvider<CompletionParameters?>() {
+                FLAG_KEY,
+                object : CompletionProvider<CompletionParameters>() {
                     override fun addCompletions(parameters: CompletionParameters,
                                                 context: ProcessingContext,
                                                 resultSet: CompletionResultSet) {
                         val project = parameters.originalFile.project
                         val getFlags = project.service<FlagStore>()
-                        for (flag in getFlags.flags.items) {
-                                resultSet.addElement(LookupElementBuilder.create(flag.key))
-                        }
+                        resultSet.addElement(LookupElementBuilder.create("my-flag-key"))
+//                        for (flag in getFlags.flags.items) {
+//                                resultSet.addElement(LookupElementBuilder.create(flag.key))
+//                        }
                     }
+
                 }
         )
     }
