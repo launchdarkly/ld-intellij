b967d7140a3063185cc0f8d73397bca44f994533
Dan O'Brien <dobrien@launchdarkly.com>
2022-02-18 13:41:16 -0500

    minor updates to target tests (#10)

--- src/test/kotlin/com/launchdarkly/intellij/toolwindow/FlagNodeTargetsTest.kt
+++ src/test/kotlin/com/launchdarkly/intellij/toolwindow/FlagNodeTargetsTest.kt
@@ -3,22 +3,57 @@ package com.launchdarkly.intellij.toolwindow
 import com.launchdarkly.api.model.FeatureFlag
 import com.launchdarkly.api.model.Target
 import com.launchdarkly.api.model.Variation
-import org.jdesktop.swingx.action.Targetable
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 internal class FlagNodeTargetsTest {
-    private var flag: FeatureFlag = createFlag()
-    private val targets = createTargets()
-    private val testTargets: FlagNodeTargets = FlagNodeTargets(flag = flag, targets)
 
     @Test
-    fun getChildren() {
-        assertEquals(2, testTargets.childCount)
+    fun hasCorrectLabelWhenVariationIsNamed() {
+        val flag: FeatureFlag = createFlag()
+        val target = Target().apply {
+            variation = 0
+            values = listOf("false")
+        }
+        val testTarget = FlagNodeTargets(flag = flag, listOf(target))
+        val expected = "variation0: 1"
+        val children = testTarget.children
+        for (child in children) {
+            assertEquals(expected, child.label)
+        }
     }
 
-    fun createFlag(flagKey: String ="test"): FeatureFlag {
+    @Test
+    fun hasCorrectLabelWhenVariationHasNoName() {
+        val flag = FeatureFlag().apply {
+            key = "test"
+            name = "test"
+            variations = listOf(Variation().apply {
+                value = false
+            })
+        }
+        val target = Target().apply {
+            variation = 0
+            values = listOf("user1", "user2")
+        }
+        val testTarget = FlagNodeTargets(flag = flag, listOf(target))
+        val expected = "false: 2"
+        val children = testTarget.children
+        for (child in children) {
+            assertEquals(expected, child.label)
+        }
+    }
+
+    @Test
+    fun returnsCorrectChildCount() {
+        val flag: FeatureFlag = createFlag()
+        val targets = createTargets()
+        val testTargets = FlagNodeTargets(flag = flag, targets)
+        assertEquals(targets.size, testTargets.childCount)
+    }
+
+    private fun createFlag(flagKey: String ="test"): FeatureFlag {
         val flag = FeatureFlag().apply {
             key = flagKey
             name = flagKey
@@ -26,26 +61,27 @@ internal class FlagNodeTargetsTest {
 
         val variation1 = Variation().apply {
             value = false
+            name = "variation0"
         }
 
         val variation2 = Variation().apply {
             value = true
         }
 
-
         flag.variations = listOf(variation1, variation2)
         return flag
     }
 
-    fun createTargets(): List<Target> {
+
+    private fun createTargets(): List<Target> {
         val target1 = Target().apply {
             variation = 0
-            values = listOf("false")
+            values = listOf("user1")
         }
 
         val target2 = Target().apply {
             variation = 1
-            values = listOf("true")
+            values = listOf("user1")
         }
 
         return listOf(target1, target2)
