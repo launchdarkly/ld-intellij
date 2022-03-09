b3942e61d656594feb196b973b75f926628b2a88
Jaz White <jwhite@launchdarkly.com>
2022-03-09 11:27:13 -0500

    Use row comment

--- src/main/kotlin/com/launchdarkly/intellij/settings/LaunchDarklyApplicationSettings.kt
+++ src/main/kotlin/com/launchdarkly/intellij/settings/LaunchDarklyApplicationSettings.kt
@@ -144,10 +144,7 @@ class LaunchDarklyApplicationConfigurable : BoundConfigurable(displayName = "Lau
                         it
                     )
                 }
-            }
-            row {
-                comment("Input the API key from your LaunchDarkly account. If you don’t have one, you must <a href=\"https://docs.launchdarkly.com/home/account-security/api-access-tokens#creating-api-access-tokens\">create an access token</a> first.")
-            }
+            }.rowComment("Input the API key from your LaunchDarkly account. If you don’t have one, you must <a href=\"https://docs.launchdarkly.com/home/account-security/api-access-tokens#creating-api-access-tokens\">create an access token</a> first.")
 
             try {
                 projectBox = if (::projectContainer.isInitialized) {
