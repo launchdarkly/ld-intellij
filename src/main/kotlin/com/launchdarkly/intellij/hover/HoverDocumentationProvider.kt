4b89c244723055e7d4a9f80d6d10b1309dceb35f
Yusinto Ngadiman <yusinto@gmail.com>
2022-03-14 10:24:53 -0700

    Update HoverDocumentationProvider.kt

--- src/main/kotlin/com/launchdarkly/intellij/hover/HoverDocumentationProvider.kt
+++ src/main/kotlin/com/launchdarkly/intellij/hover/HoverDocumentationProvider.kt
@@ -14,7 +14,6 @@ import com.launchdarkly.intellij.FlagStore
 import com.launchdarkly.intellij.Utils
 import com.launchdarkly.intellij.coderefs.FlagAliases
 import com.launchdarkly.intellij.featurestore.FlagConfiguration
-import com.launchdarkly.intellij.settings.LaunchDarklyApplicationConfig
 import com.mitchellbosecke.pebble.PebbleEngine
 import java.io.StringWriter
 
@@ -62,13 +61,11 @@ class HoverDocumentationProvider : AbstractDocumentationProvider() {
                 PlatformPatterns.psiElement().notEmpty()
             )
         ) return null
-        val getFlags = element.project.service<FlagStore>()
 
+        val getFlags = element.project.service<FlagStore>()
         if (getFlags.flags.items == null) return null
 
         val getAliases = element.project.service<FlagAliases>()
-        val settings = LaunchDarklyApplicationConfig.getInstance().ldState
-
         var flag: FeatureFlag? =
             getFlags.flags?.items?.find { element.text.contains(it.key) }
         var alias: String?
@@ -76,84 +73,13 @@ class HoverDocumentationProvider : AbstractDocumentationProvider() {
             alias = getAliases.aliases[element.text.removeSurrounding("\"")]
             flag = getFlags.flags.items.find { it.key == alias }
         }
+
         // TODO: gracefully handle API call working and Datastore being unavailable
         if (flag != null) {
             val env: FlagConfiguration = getFlags.flagConfigs[flag.key]
                 ?: FlagConfiguration(flag.key, null, null, listOf(), listOf(), arrayOf(), false, -1)
-            val prereqs = if (env.prerequisites.isNotEmpty()) {
-                "<b>Prerequisites</b> ${env.prerequisites.size} • "
-            } else ""
-            val rules = if (env.rules.isNotEmpty()) {
-                "<b>Rules</b> ${env.rules.size}<br />"
-            } else if (env.targets.isNotEmpty()) {
-                " •"
-            } else {
-                ""
-            }
-            var targets = ""
-            if (env.targets.isNotEmpty()) {
-                targets += "<b>Targets</b><br /> "
-                env.targets.forEachIndexed { i, t ->
-                    targets += "${flag.variations[t.variation as Int].name ?: flag.variations[t.variation as Int].value} ${t.values.size}"
-                    if (i != env.targets.lastIndex) {
-                        targets += " \u2022 "
-                    }
-                }
-                targets += "<br />"
-            }
-            var buildEnvString = ""
-            if (prereqs.length > 1) {
-                buildEnvString += "$prereqs "
-            }
-            if (rules.length > 1) {
-                buildEnvString += rules
-            }
-            if (targets != "") {
-                buildEnvString += targets
-            }
-//            result.append("<html>")
-//            if (env.version === -1) {
-//                result.append("<b>FLAG TARGETING INFORMATION IS NOT AVAILABLE. Below Values are placeholders</b><br />")
-//            }
-//            result.append("<b>LaunchDarkly Feature Flag \u2022 ${flag.name ?: flag.key}</b><br />")
-//            result.append("<a href=\"${settings.baseUri}${flag.environments[settings.environment]!!.site.href}\">Open In LaunchDarkly</a><br />")
-//
-//            val state = if (env.version === -1) {
-//                "Disconnect"
-//            } else if (env.on) {
-//                "On"
-//            } else {
-//                "Off"
-//            }
-//            result.append("Enabled: $state<br />")
-//            result.append("${flag.description}<br />")
-//            result.append(buildEnvString)
-//            result.append("<br /><b>Variations ${if (env.fallthrough?.rollout != null) " ◆ Rollout Configured" else ""}</b><br />")
-//            flag.variations.mapIndexed { i, it ->
-//                val rolloutPercentage: Double = if (env.fallthrough?.rollout != null) {
-//                    val rollout = env.fallthrough?.rollout
-//                    val foundVariation = rollout!!.variations.filter { it.variation == i }
-//                    (foundVariation[0].weight.toDouble() / 1000)
-//                } else -1.000
-//                var variationOut = "$i"
-//                if (it.name != "" && it.name != null) {
-//                    variationOut += " ◆ ${it.name}"
-//                }
-//                variationOut += " ◆ ${if (rolloutPercentage != null && rolloutPercentage != -1.000) "Rollout $rolloutPercentage% ◆ " else ""}<code>Return value:</code> <code>${it.value}</code><br />"
-//                result.append(variationOut)
-//                if (env.offVariation != null && env.offVariation == i) {
-//                    result.append("<p><b>Off Variation</b></p>")
-//                }
-//                if (env.fallthrough?.variation != null && env.fallthrough?.variation == i) {
-//                    result.append("<p><b>Fallthrough Variation</b></p>")
-//                }
-//                if (it.description != "" && it.description != null) {
-//                    result.append("<p>${it.description ?: ""}</p><br />")
-//                }
-//            }
-//            result.append("</html>")
 
-            // Build the variations object we can use in the html template
+            // Construct view models in kotlin so we can use them in the html template
             // to minimise logic operations in pebble (pain!)
             val variationsViewModel = ArrayList<Variation>()
             flag.variations.forEach { v ->
@@ -173,7 +99,6 @@ class HoverDocumentationProvider : AbstractDocumentationProvider() {
                 put("variations", variationsViewModel)
             }
 
-            // Pass data to html template to be rendered
             val template = PebbleEngine.Builder().build().getTemplate("htmlTemplates/flagKeyHover.html")
             val writer = StringWriter()
             template.evaluate(writer, mapOf("flag" to flagViewModel))
