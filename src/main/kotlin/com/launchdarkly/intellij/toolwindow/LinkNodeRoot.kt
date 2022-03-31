6ce11ac6b2fea3e159f80c8620e0edb7defdb9be
Jaz White <jwhite@launchdarkly.com>
2022-03-31 13:55:58 -0400

    [sc-146473] add query params for Links in IntelliJ (#45)
    * update user agent for api client

--- src/main/kotlin/com/launchdarkly/intellij/toolwindow/LinkNodeRoot.kt
+++ src/main/kotlin/com/launchdarkly/intellij/toolwindow/LinkNodeRoot.kt
@@ -2,6 +2,7 @@ package com.launchdarkly.intellij.toolwindow
 
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ui.treeStructure.SimpleNode
+import com.launchdarkly.intellij.Utils
 import com.launchdarkly.intellij.settings.LDSettings
 
 class LinkNodeRoot(private val settings: LDSettings) :
@@ -13,43 +14,43 @@ class LinkNodeRoot(private val settings: LDSettings) :
             myChildren.add(
                 LinkNodeBase(
                     "Flags",
-                    "${settings.baseUri}/${settings.project}/${settings.environment}/features"
+                    "${settings.baseUri}/${settings.project}/${settings.environment}/features?${Utils.getQueryParams()}"
                 )
             )
             myChildren.add(
                 LinkNodeBase(
                     "Segments",
-                    "${settings.baseUri}/${settings.project}/${settings.environment}/segments"
+                    "${settings.baseUri}/${settings.project}/${settings.environment}/segments?${Utils.getQueryParams()}"
                 )
             )
             myChildren.add(
                 LinkNodeBase(
                     "Users",
-                    "${settings.baseUri}/${settings.project}/${settings.environment}/users"
+                    "${settings.baseUri}/${settings.project}/${settings.environment}/users?${Utils.getQueryParams()}"
                 )
             )
             myChildren.add(
                 LinkNodeBase(
                     "Debugger",
-                    "${settings.baseUri}/${settings.project}/${settings.environment}/debugger"
+                    "${settings.baseUri}/${settings.project}/${settings.environment}/debugger?${Utils.getQueryParams()}"
                 )
             )
             myChildren.add(
                 LinkNodeBase(
                     "Experiments",
-                    "${settings.baseUri}/${settings.project}/${settings.environment}/experiments"
+                    "${settings.baseUri}/${settings.project}/${settings.environment}/experiments?${Utils.getQueryParams()}"
                 )
             )
             myChildren.add(
                 LinkNodeBase(
                     "Audit Log",
-                    "${settings.baseUri}/${settings.project}/${settings.environment}/audit"
+                    "${settings.baseUri}/${settings.project}/${settings.environment}/audit?${Utils.getQueryParams()}"
                 )
             )
             myChildren.add(
                 LinkNodeBase(
                     "Flag Comparison",
-                    "${settings.baseUri}/${settings.project}/${settings.environment}/features/compare"
+                    "${settings.baseUri}/${settings.project}/${settings.environment}/features/compare?${Utils.getQueryParams()}"
                 )
             )
             // TODO: Fix selector so we can uncomment this
@@ -62,13 +63,13 @@ class LinkNodeRoot(private val settings: LDSettings) :
             myChildren.add(
                 LinkNodeBase(
                     "Open Documentation",
-                    "https://docs.launchdarkly.com"
+                    "https://docs.launchdarkly.com?${Utils.getQueryParams()}"
                 )
             )
             myChildren.add(
                 LinkNodeBase(
                     "Open API Documentation",
-                    "https://apidocs.launchdarkly.com"
+                    "https://apidocs.launchdarkly.com?${Utils.getQueryParams()}"
                 )
             )
         }
