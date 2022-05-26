4f18882f28e40b3440f0586bb9b8c9dcfee8318c
Jacob Allen <jacobscottallen@gmail.com>
2022-05-26 14:25:17 -0700

    change labels (#48)
    * change labels
    
    * change function name
    
    * change test names
    
    * change additional fallthrough wording

--- src/main/kotlin/com/launchdarkly/intellij/toolwindow/FlagNodeParent.kt
+++ src/main/kotlin/com/launchdarkly/intellij/toolwindow/FlagNodeParent.kt
@@ -34,8 +34,8 @@ class FlagNodeParent(private var viewModel: FlagNodeViewModel) : SimpleNode() {
         if (viewModel.hasPrereqs) children.add(FlagNodePrerequisites(viewModel.prereqFlags, viewModel.flags))
         if (viewModel.hasTargets) children.add(FlagNodeTargets(flag, viewModel.targets))
         if (viewModel.hasRules) children.add(FlagNodeBase("Rules: ${viewModel.numRules}", LDIcons.RULES))
-        if (viewModel.hasFallthrough) children.add(FlagNodeFallthrough(flag, viewModel.flagConfig!!))
-        if (viewModel.hasOffVariation) children.add(FlagNodeBase("Off Variation: ${viewModel.offVariation}", LDIcons.OFF_VARIATION))
+        if (viewModel.hasFallthrough) children.add(FlagNodeDefaultRule(flag, viewModel.flagConfig!!))
+        if (viewModel.hasOffVariation) children.add(FlagNodeBase("Off variation: ${viewModel.offVariation}", LDIcons.OFF_VARIATION))
         if (viewModel.hasTags) children.add(FlagNodeTags(viewModel.tags))
     }
 
