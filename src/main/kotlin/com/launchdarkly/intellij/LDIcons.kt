9153273f368070c93159898a2c71dc6702d7d419
Sunny Guduru <sguduru@launchdarkly.com>
2023-12-29 11:55:45 -0800

    Fix tests for 2023.2 update (#70)

--- src/main/kotlin/com/launchdarkly/intellij/LDIcons.kt
+++ src/main/kotlin/com/launchdarkly/intellij/LDIcons.kt
@@ -5,42 +5,42 @@ import com.intellij.openapi.util.IconLoader
 class LDIcons {
     companion object {
         @JvmField
-        val DEFAULT_VARIATION = IconLoader.getIcon("/icons/variationDefault.svg", javaClass)
+        val DEFAULT_VARIATION = IconLoader.getIcon("/icons/variationDefault.svg", LDIcons::class.java)
 
         @JvmField
-        val DESCRIPTION = IconLoader.getIcon("/icons/description.svg", javaClass)
+        val DESCRIPTION = IconLoader.getIcon("/icons/description.svg", LDIcons::class.java)
 
         @JvmField
-        val LOGO = IconLoader.getIcon("/icons/logo.svg", javaClass)
+        val LOGO = IconLoader.getIcon("/icons/logo.svg", LDIcons::class.java)
 
         @JvmField
-        val FLAG_KEY = IconLoader.getIcon("/icons/flagKey.svg", javaClass)
+        val FLAG_KEY = IconLoader.getIcon("/icons/flagKey.svg", LDIcons::class.java)
 
         @JvmField
-        val OFF_VARIATION = IconLoader.getIcon("/icons/offVariation.svg", javaClass)
+        val OFF_VARIATION = IconLoader.getIcon("/icons/offVariation.svg", LDIcons::class.java)
 
         @JvmField
-        val PREREQUISITE = IconLoader.getIcon("/icons/prereq.svg", javaClass)
+        val PREREQUISITE = IconLoader.getIcon("/icons/prereq.svg", LDIcons::class.java)
 
         @JvmField
-        val RULES = IconLoader.getIcon("/icons/rules.svg", javaClass)
+        val RULES = IconLoader.getIcon("/icons/rules.svg", LDIcons::class.java)
 
         @JvmField
-        val TAGS = IconLoader.getIcon("/icons/tags.svg", javaClass)
+        val TAGS = IconLoader.getIcon("/icons/tags.svg", LDIcons::class.java)
 
         @JvmField
-        val TARGETS = IconLoader.getIcon("/icons/targets.svg", javaClass)
+        val TARGETS = IconLoader.getIcon("/icons/targets.svg", LDIcons::class.java)
 
         @JvmField
-        val TOGGLE_OFF = IconLoader.getIcon("/icons/toggleoff.svg", javaClass)
+        val TOGGLE_OFF = IconLoader.getIcon("/icons/toggleoff.svg", LDIcons::class.java)
 
         @JvmField
-        val TOGGLE_ON = IconLoader.getIcon("/icons/toggleon.svg", javaClass)
+        val TOGGLE_ON = IconLoader.getIcon("/icons/toggleon.svg", LDIcons::class.java)
 
         @JvmField
-        val TOGGLE_DISCONNECTED = IconLoader.getIcon("/icons/toggledisc.svg", javaClass)
+        val TOGGLE_DISCONNECTED = IconLoader.getIcon("/icons/toggledisc.svg", LDIcons::class.java)
 
         @JvmField
-        val VARIATION = IconLoader.getIcon("/icons/variation.svg", javaClass)
+        val VARIATION = IconLoader.getIcon("/icons/variation.svg", LDIcons::class.java)
     }
 }
